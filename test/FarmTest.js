bfd47f8865f19a32a2e30e816b8e2c59ae7a315a
Ankit Sharma <61593608+Asharma171@users.noreply.github.com>
2021-09-24 14:09:51 +0530

    Update FarmTest.js

--- test/FarmTest.js
+++ test/FarmTest.js
@@ -1,10 +1,5 @@
-const {
-  BN, // Big Number support
-  constants, // Common constants, like the zero address and largest integers
-  expectEvent, // Assertions for emitted events
-  expectRevert, // Assertions for transactions that should fail
-  time,
-} = require("@openzeppelin/test-helpers");
+const {BN, constants, expectEvent, expectRevert, time,} 
+  = require("@openzeppelin/test-helpers");
 
 const { assert, expect } = require("chai");
 const { increase } = require("@openzeppelin/test-helpers/src/time");
@@ -13,132 +8,207 @@ const RemitFarm = artifacts.require("RemitVaultFarmEth");
 const AStoken = artifacts.require("AStoken");
 
 contract("Remit Farm", () => {
-  it("", async () => {
-    const remit = await Remit.deployed();
-    const remitfarm = await RemitFarm.deployed();
-    const tokenOther = await AStoken.deployed();
-
-    assert(remit.address !== "");
-    assert(remitfarm.address !== "");
-    assert(tokenOther.address !== "");
-  });
 
   beforeEach(async function () {
     remit = await Remit.new();
     remitfarm = await RemitFarm.new()
-    tokenOther = await AStoken.new();
+    ASt = await AStoken.new();
     accounts = await web3.eth.getAccounts();
   });
 
-  describe("[Testcase 1: Deposit  tokens ]", () => {
-    it("Deposit ", async () => {
-      await remit.mintCirculationSupply(accounts[0],web3.utils.toWei("300","ether"));
-      await remit.setFarmAddress(remitfarm.address);
-      await remitfarm.setRewardTokenAddress(remit.address);
-      await remitfarm.setDepositTokenAddress(tokenOther.address);
-      await tokenOther.transfer(accounts[2],web3.utils.toWei("100","ether"));
-      await tokenOther.approve(remitfarm.address,web3.utils.toWei("55","ether"),{from:accounts[2]});
-      await remitfarm.deposit(web3.utils.toWei("20","ether"),{from : accounts[2]});
-      await remitfarm.deposit(web3.utils.toWei("10","ether"),{from : accounts[2]});
-      var actual = await remitfarm.depositedTokens( accounts[2]);
-      var expected = web3.utils.toWei("30","ether");
-      assert.equal(actual,expected);
+ 
 
-      await time.increase(time.duration.days(45));
+  it("Deposit ", async () => {
+    await remit.mintCirculationSupply(accounts[0],web3.utils.toWei("300","ether"));
+    await remit.setFarmAddress(remitfarm.address);
+    await remitfarm.setRewardTokenAddress(remit.address);
+    await remitfarm.setDepositTokenAddress(ASt.address);
+    await ASt.transfer(accounts[2],web3.utils.toWei("100","ether"));
+    await ASt.approve(remitfarm.address,web3.utils.toWei("55","ether"),{from:accounts[2]});
+    await remitfarm.deposit(web3.utils.toWei("20","ether"),{from : accounts[2]});
+    await remitfarm.deposit(web3.utils.toWei("10","ether"),{from : accounts[2]});
+    var actual = await remitfarm.depositedTokens( accounts[2]);
+    var expected = web3.utils.toWei("30","ether");
+    assert.equal(actual,expected);
 
-      await remitfarm.deposit(web3.utils.toWei("15","ether"),{from : accounts[2]});
+    await time.increase(time.duration.days(45));
+
+    await remitfarm.deposit(web3.utils.toWei("15","ether"),{from : accounts[2]});
       
-      actual = await remit.stakeFarmSupply();
-      expected = web3.utils.toWei("350000","ether");
-      assert.notEqual(actual,expected);
-
-    });
+    actual = await remit.stakeFarmSupply();
+    expected = web3.utils.toWei("350000","ether");
+    assert.notEqual(actual,expected);
   });
-
-  describe("[Testcase 2: Withdraw tokens ]", () => {
-    it("withdraw", async () => {
-        await remit.mintCirculationSupply(accounts[0],web3.utils.toWei("300","ether"));
-        await remit.setFarmAddress(remitfarm.address);
-        await remitfarm.setRewardTokenAddress(remit.address);
-        await remitfarm.setDepositTokenAddress(tokenOther.address);
-        await tokenOther.transfer(accounts[2],web3.utils.toWei("250","ether"));
-        await tokenOther.approve(remitfarm.address,web3.utils.toWei("100","ether"),{from:accounts[2]});
-        await remitfarm.deposit(web3.utils.toWei("40","ether"),{from : accounts[2]});
-        await remitfarm.deposit(web3.utils.toWei("30","ether"),{from : accounts[2]});
-
-        await time.increase(time.duration.days(56));
-        
-        await remitfarm.withdraw(web3.utils.toWei("29","ether"),{from : accounts[2]});
-
-        var actual = await remitfarm.totalTokens();
-        var expected = web3.utils.toWei("41","ether");
-        assert.equal(actual,expected);
-    } );
-  });
-
-  describe("[Testcase 3: Deposit non trusted tokens ]", () => {
-    it("Deposit ", async () => {
-      await remit.mintCirculationSupply(accounts[0],web3.utils.toWei("400","ether"));
-      await remit.setFarmAddress(remitfarm.address);
-      await remitfarm.setRewardTokenAddress(remit.address);
-      await tokenOther.transfer(accounts[6],web3.utils.toWei("200","ether"));
-      await tokenOther.approve(remitfarm.address,web3.utils.toWei("200","ether"),{from:accounts[6]});
-      try{
-        await remitfarm.deposit(web3.utils.toWei("80","ether"),{from : accounts[6]});
-      }
-      catch{
-        var actual = await remitfarm.depositedTokens( accounts[6]);
-        var expected = 0;
-        assert.equal(actual,expected);
-      }
-    }); 
-  }); 
   
-  describe("[Testcase 4: Emergency Withdraw ]", () => {
-    it("Emergency Withdraw", async () => {
-      await remit.mintCirculationSupply(accounts[0],web3.utils.toWei("200","ether"));
-      await remit.setFarmAddress(remitfarm.address);
-      await remitfarm.setRewardTokenAddress(remit.address);
-      await remitfarm.setDepositTokenAddress(tokenOther.address);
-      await tokenOther.transfer(accounts[2],web3.utils.toWei("80","ether"));
-      await tokenOther.approve(remitfarm.address,web3.utils.toWei("65","ether"),{from:accounts[2]});
-      await remitfarm.deposit(web3.utils.toWei("40","ether"),{from : accounts[2]});
 
-      await time.increase(time.duration.hours(84));
-     
-      await remitfarm.emergencyWithdraw(web3.utils.toWei("28","ether"),{from : accounts[2]});
-      var actual = await remitfarm.totalEarnedTokens(accounts[2]);
+  
+  it("withdraw", async () => {
+    await remit.mintCirculationSupply(accounts[0],web3.utils.toWei("300","ether"));
+    await remit.setFarmAddress(remitfarm.address);
+    await remitfarm.setRewardTokenAddress(remit.address);
+    await remitfarm.setDepositTokenAddress(ASt.address);
+    await ASt.transfer(accounts[2],web3.utils.toWei("250","ether"));
+    await ASt.approve(remitfarm.address,web3.utils.toWei("100","ether"),{from:accounts[2]});
+    await remitfarm.deposit(web3.utils.toWei("40","ether"),{from : accounts[2]});
+    await remitfarm.deposit(web3.utils.toWei("30","ether"),{from : accounts[2]});
+
+    await time.increase(time.duration.days(56));
+        
+    await remitfarm.withdraw(web3.utils.toWei("29","ether"),{from : accounts[2]});
+
+    var actual = await remitfarm.totalTokens();
+    var expected = web3.utils.toWei("41","ether");
+    assert.equal(actual,expected);
+  });
+  
+
+  
+  it("Deposit non trusted token ", async () => {
+    await remit.mintCirculationSupply(accounts[0],web3.utils.toWei("400","ether"));
+    await remit.setFarmAddress(remitfarm.address);
+    await remitfarm.setRewardTokenAddress(remit.address);
+    await ASt.transfer(accounts[6],web3.utils.toWei("200","ether"));
+    await ASt.approve(remitfarm.address,web3.utils.toWei("200","ether"),{from:accounts[6]});
+    try{
+      await remitfarm.deposit(web3.utils.toWei("80","ether"),{from : accounts[6]});
+    }
+    catch{
+      var actual = await remitfarm.depositedTokens( accounts[6]);
       var expected = 0;
       assert.equal(actual,expected);
-    });
+    }
+  }); 
+  
+  
+  
+  it("Emergency Withdraw", async () => {
+    await remit.mintCirculationSupply(accounts[0],web3.utils.toWei("200","ether"));
+    await remit.setFarmAddress(remitfarm.address);
+    await remitfarm.setRewardTokenAddress(remit.address);
+    await remitfarm.setDepositTokenAddress(ASt.address);
+    await ASt.transfer(accounts[2],web3.utils.toWei("80","ether"));
+    await ASt.approve(remitfarm.address,web3.utils.toWei("65","ether"),{from:accounts[2]});
+    await remitfarm.deposit(web3.utils.toWei("40","ether"),{from : accounts[2]});
+
+    await time.increase(time.duration.hours(84));
+    
+    await remitfarm.emergencyWithdraw(web3.utils.toWei("28","ether"),{from : accounts[2]});
+    var actual = await remitfarm.totalEarnedTokens(accounts[2]);
+    var expected = 0;
+    assert.equal(actual,expected);
   });
 
-  describe("[Testcase 5: To deposit and withdraw ]", () => {
-    it("Deposit and Withdraw Tokens", async () => {
-      await remit.mintCirculationSupply(accounts[0],web3.utils.toWei("500","ether"));
-      await remit.setFarmAddress(remitfarm.address);
-      await remitfarm.setRewardTokenAddress(remit.address);
-      await remitfarm.setDepositTokenAddress(tokenOther.address);
 
-      await tokenOther.transfer(accounts[5],web3.utils.toWei("200","ether"));
-      await tokenOther.approve(remitfarm.address,web3.utils.toWei("155","ether"),{from:accounts[5]});
-      await remitfarm.deposit(web3.utils.toWei("40","ether"),{from : accounts[5]});
+  
+  it("Deposit and Withdraw Tokens", async () => {
+    await remit.mintCirculationSupply(accounts[0],web3.utils.toWei("500","ether"));
+    await remit.setFarmAddress(remitfarm.address);
+    await remitfarm.setRewardTokenAddress(remit.address);
+    await remitfarm.setDepositTokenAddress(ASt.address);
 
-      await tokenOther.transfer(accounts[6],web3.utils.toWei("45","ether"));
-      await tokenOther.approve(remitfarm.address,web3.utils.toWei("30","ether"),{from:accounts[6]});
-      await remitfarm.deposit(web3.utils.toWei("25","ether"),{from : accounts[6]});
+    await ASt.transfer(accounts[5],web3.utils.toWei("200","ether"));
+    await ASt.approve(remitfarm.address,web3.utils.toWei("155","ether"),{from:accounts[5]});
+    await remitfarm.deposit(web3.utils.toWei("40","ether"),{from : accounts[5]});
 
-      await remitfarm.deposit(web3.utils.toWei("10","ether"),{from : accounts[5]});
+    await remitfarm.deposit(web3.utils.toWei("10","ether"),{from : accounts[5]});
 
-      await time.increase(time.duration.days(56));
-          
-      await remitfarm.withdraw(web3.utils.toWei("18","ether"),{from : accounts[5]});
+    await time.increase(time.duration.days(56));
+        
+    await remitfarm.withdraw(web3.utils.toWei("18","ether"),{from : accounts[5]});
 
-      var actual = await remit.stakeFarmSupply();
-      var expected = web3.utils.toWei("350000","ether");
-      assert.notEqual(actual,expected);
+    var actual = await remit.stakeFarmSupply();
+    var expected = web3.utils.toWei("350000","ether");
+    assert.notEqual(actual,expected);
 
+  });
+
+
+  it("getNumberOfHolders", async () => {
+    await remit.mintCirculationSupply(accounts[0],web3.utils.toWei("300","ether"));
+    await remit.setFarmAddress(remitfarm.address);
+    await remitfarm.setRewardTokenAddress(remit.address);
+    await remitfarm.setDepositTokenAddress(ASt.address);
+    await ASt.transfer(accounts[2],web3.utils.toWei("100","ether"));
+    await ASt.approve(remitfarm.address,web3.utils.toWei("55","ether"),{from:accounts[2]});
+    await remitfarm.deposit(web3.utils.toWei("20","ether"),{from : accounts[2]});
+    
+    it("No. of holders/stakers should increase", async () => {
+    assert.notEqual(getNumberOfHolders,0);	
     });
+    
+  
+  });
+  
+  
+  it("withdraw won't work before cliff time", async () => {
+    await remit.mintCirculationSupply(accounts[0],web3.utils.toWei("300","ether"));
+    await remit.setFarmAddress(remitfarm.address);
+    await remitfarm.setRewardTokenAddress(remit.address);
+    await remitfarm.setDepositTokenAddress(ASt.address);
+    await ASt.transfer(accounts[4],web3.utils.toWei("100","ether"));
+    await ASt.approve(remitfarm.address,web3.utils.toWei("100","ether"),{from:accounts[4]});
+    await remitfarm.deposit(web3.utils.toWei("89","ether"),{from : accounts[4]});
+    
+    try{
+      await remitfarm.withdraw(web3.utils.toWei("65","ether"),{from : accounts[4]});
+    }
+    catch{
+    }
+    var actual = await remitfarm.totalEarnedTokens(accounts[4]);
+    var expected = 0;
+    assert.equal(actual,expected);
+  
+  });
+
+  
+  it("totalEarnedTokens", async () => {
+    await remit.mintCirculationSupply(accounts[0],web3.utils.toWei("400","ether"));
+    await remit.setFarmAddress(remitfarm.address);
+    await remitfarm.setRewardTokenAddress(remit.address);
+    await remitfarm.setDepositTokenAddress(ASt.address);
+    await ASt.transfer(accounts[2],web3.utils.toWei("100","ether"));
+    await ASt.approve(remitfarm.address,web3.utils.toWei("55","ether"),{from:accounts[2]});
+    await remitfarm.deposit(web3.utils.toWei("20","ether"),{from : accounts[2]});
+    var actual = await remitfarm.totalEarnedTokens(accounts[2]);
+
+    it("total earned tokens should increase ", async () => {
+      assert.notEqual(totalEarnedTokens,0);	
+    });
+    
+  });
+
+  it("Deposite won't work if there is NO circulation supply ", async () => {
+    await remit.mintCirculationSupply(accounts[0],web3.utils.toWei("0","ether"));
+    await remit.setFarmAddress(remitfarm.address);
+    await remitfarm.setRewardTokenAddress(remit.address);
+    await remitfarm.setDepositTokenAddress(ASt.address);
+    try{
+      await remitfarm.deposit(web3.utils.toWei("80","ether"),{from : accounts[2]});
+    }
+    catch{
+      var actual = await remitfarm.depositedTokens( accounts[2]);
+      var expected = 0;
+      assert.equal(actual,expected);
+    }
+  });
+  
+  it("Can't withdraw bcz of insufficient token ", async () => {
+    await remit.mintCirculationSupply(accounts[0],web3.utils.toWei("400","ether"));
+    await remit.setFarmAddress(remitfarm.address);
+    await remitfarm.setRewardTokenAddress(remit.address);
+    await remitfarm.setDepositTokenAddress(ASt.address);
+    await ASt.transfer(accounts[2],web3.utils.toWei("100","ether"));
+    await ASt.approve(remitfarm.address,web3.utils.toWei("55","ether"),{from:accounts[2]});
+    await remitfarm.deposit(web3.utils.toWei("20","ether"),{from : accounts[2]});
+    
+    try{
+      await remitfarm.withdraw(web3.utils.toWei("65","ether"),{from : accounts[4]});
+    }
+    catch{
+    }
+    var actual = await remitfarm.totalEarnedTokens(accounts[4]);
+    var expected = 0;
+    assert.equal(actual,expected);
   });
 
 });
